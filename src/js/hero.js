630f6a40cab62d87e0a3e442ea71489f13a8b445
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-29 03:18:37 +0300

    hero js animation

---
+++ src/js/hero.js
@@ -0,0 +1,23 @@
+const elements = {
+  heroBack: document.querySelector('#background'),
+};
+
+const backgroundImages = [
+  'url(../img/hero/hero-1.png)',
+  'url(../img/hero/hero-2.png)',
+  'url(../img/hero/hero-3.png)',
+  'url(../img/hero/hero-4.png)',
+  'url(../img/hero/hero-6.png)',
+];
+let currentIndex = 0;
+
+function changeBackground() {
+  elements.heroBack.opacity = 0;
+  setTimeout(function () {
+    elements.heroBack.style.backgroundImage = backgroundImages[currentIndex];
+    elements.heroBack.style.opacity = 1;
+  }, 3000);
+  currentIndex = (currentIndex + 1) % backgroundImages.length;
+}
+
+setInterval(changeBackground, 3000);
