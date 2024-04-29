9384b08755e4fb986f7d87c933339b86cb64aecd
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-29 17:30:47 +0300

    cover animation

---
+++ src/js/covers.js
@@ -0,0 +1,20 @@
+const hiddenElements = document.querySelectorAll('.marquee-inner');
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      const lines = entry.target.querySelectorAll('.marquee__line');
+      lines.forEach(line => {
+        if (entry.isIntersecting) {
+          line.style.animationPlayState = 'running';
+        } else {
+          line.style.animationPlayState = 'paused';
+        }
+      });
+    });
+  },
+  {
+    threshold: 0.9,
+  }
+);
+
+hiddenElements.forEach(el => observer.observe(el));
