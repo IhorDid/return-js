f1c3a31988eb1dc92b6cd8e7bd0feadd86082b88
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-29 04:02:49 +0300

    attemt 6

--- src/js/hero.js
+++ src/js/hero.js
@@ -8,11 +8,11 @@ const elements = {
 };
 
 const backgroundImages = [
-  '../img/hero/hero-1.png',
-  '../img/hero/hero-2.png',
-  '../img/hero/hero-3.png',
-  '../img/hero/hero-4.png',
-  '../img/hero/hero-6.png',
+  '/img/hero/hero-1.png',
+  '/img/hero/hero-2.png',
+  '/img/hero/hero-3.png',
+  '/img/hero/hero-4.png',
+  '/img/hero/hero-6.png',
 ];
 let currentIndex = 0;
 
