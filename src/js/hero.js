b55574daad1c8c039879f9c026452abb07f4e3e2
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-29 03:52:04 +0300

    attemt 3 animation

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,18 +1,18 @@
-import heroOne from '../img/hero/hero-1.png';
-import heroTwo from '../img/hero/hero-2.png';
-import heroThree from '../img/hero/hero-3.png';
-import heroFour from '../img/hero/hero-4.png';
-import heroFive from '../img/hero/hero-6.png';
+// import heroOne from '../img/hero/hero-1.png';
+// import heroTwo from '../img/hero/hero-2.png';
+// import heroThree from '../img/hero/hero-3.png';
+// import heroFour from '../img/hero/hero-4.png';
+// import heroFive from '../img/hero/hero-6.png';
 const elements = {
   heroBack: document.querySelector('#background'),
 };
 
 const backgroundImages = [
-  `url(${heroOne})`,
-  `url(${heroTwo})`,
-  `url(${heroThree})`,
-  `url(${heroFour})`,
-  `url(${heroFive})`,
+  `url(/img/hero/hero-1.png)`,
+  `url(/img/hero/hero-2.png)`,
+  `url(/img/hero/hero-3.png)`,
+  `url(/img/hero/hero-4.png)`,
+  `url(/img/hero/hero-6.png)`,
 ];
 let currentIndex = 0;
 
