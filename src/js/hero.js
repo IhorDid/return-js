b777d4f1ff71a8d1f0ad43ba46ba76bbeee7e4c8
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-29 04:00:07 +0300

    attemt5

--- src/js/hero.js
+++ src/js/hero.js
@@ -8,18 +8,19 @@ const elements = {
 };
 
 const backgroundImages = [
-  'url(/img/hero/hero-1.png)',
-  'url(/img/hero/hero-2.png)',
-  'url(/img/hero/hero-3.png)',
-  'url(/img/hero/hero-4.png)',
-  'url(/img/hero/hero-6.png)',
+  '../img/hero/hero-1.png',
+  '../img/hero/hero-2.png',
+  '../img/hero/hero-3.png',
+  '../img/hero/hero-4.png',
+  '../img/hero/hero-6.png',
 ];
 let currentIndex = 0;
 
 function changeBackground() {
   elements.heroBack.opacity = 0;
   setTimeout(function () {
-    elements.heroBack.style.backgroundImage = backgroundImages[currentIndex];
+    elements.heroBack.style.backgroundImage =
+      'url(' + backgroundImages[currentIndex] + ')';
     elements.heroBack.style.opacity = 1;
   }, 2000);
   currentIndex = (currentIndex + 1) % backgroundImages.length;
