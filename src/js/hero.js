c4f2c7e93679eef88945e35952aaf6f336f06b00
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-29 04:11:05 +0300

    attemt 8

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,18 +1,18 @@
-// import heroOne from '../img/hero/hero-1.png';
-// import heroTwo from '../img/hero/hero-2.png';
-// import heroThree from '../img/hero/hero-3.png';
-// import heroFour from '../img/hero/hero-4.png';
-// import heroFive from '../img/hero/hero-6.png';
+import heroOne from '../img/hero/hero-1.png';
+import heroTwo from '../img/hero/hero-2.png';
+import heroThree from '../img/hero/hero-3.png';
+import heroFour from '../img/hero/hero-4.png';
+import heroFive from '../img/hero/hero-6.png';
 const elements = {
   heroBack: document.querySelector('#background'),
 };
 
 const backgroundImages = [
-  './img/hero/hero-1.png',
-  './img/hero/hero-2.png',
-  './img/hero/hero-3.png',
-  './img/hero/hero-4.png',
-  './img/hero/hero-6.png',
+  `${heroOne}`,
+  `${heroTwo}`,
+  `${heroThree}`,
+  `${heroFour}`,
+  `${heroFive}`,
 ];
 let currentIndex = 0;
 
