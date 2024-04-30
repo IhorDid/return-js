359221017fbdec92e21ec75beec6328dd1cb6b18
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-04-30 08:46:13 +0300

    update covers

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,19 +1,19 @@
-const marqueeContainer = document.querySelector('.marquee-inner');
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    const lines = entry.target.querySelectorAll('.marquee__line');
-    lines.forEach(line => {
-      if (entry.isIntersecting) {
-        line.style.animationPlayState = 'paused'; // Зупиняємо анімацію
-      } else {
-        line.style.animationPlayState = 'running'; // Відновлюємо анімацію
-      }
+const hiddenElements = document.querySelectorAll('.marquee-inner');
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      const lines = entry.target.querySelectorAll('.marquee__line');
+      lines.forEach(line => {
+        if (entry.isIntersecting) {
+          line.style.animationPlayState = 'running';
+        } else {
+          line.style.animationPlayState = 'paused';
+        }
+      });
     });
-  });
-}, {
-  threshold: 0.9,
-});
-
-
-
-
+  },
+  {
+    threshold: 0.4,
+  }
+);
+hiddenElements.forEach(el => observer.observe(el));
